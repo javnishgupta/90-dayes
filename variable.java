3ddfb0cabb860202c10ece11376344d3d5ee8b42
Jatin gupta <javnishgupta@gmail.com>
2024-10-11 15:54:43 +0530

    Basic variable
    This file has a basic function of addition, multiplication, division, and integer in a program

---
+++ variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10; 
+        int b = 5;
+        int sum = a+b;
+        System.out.println(sum);
+        int diff = a-b; 
+        System.out.println(diff);
+        int ans = a * b / a - b;
+        System.out.println(ans);
+        int ans1 = ( a * b ) / ( a - b );
+        System.out.println(ans1);
+    }
+}
+
+                                                                                
