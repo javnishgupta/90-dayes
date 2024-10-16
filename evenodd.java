ddb094786fb64ea4737647ca58ff07ccbc757487
Jatin gupta <javnishgupta@gmail.com>
2024-10-16 13:39:46 +0530

    add odd or even program

--- evenodd.java
+++ evenodd.java
@@ -2,13 +2,21 @@ import java.util.*;
 public class evenodd {
     public static void main(String[] args) {
         Scanner sc= new Scanner (System.in);
-        int age = sc.nextInt();
-        if (age>=18){
-            System.out.println("you are an adult");
+        //int age = sc.nextInt();
+        //if (age>=18){
+          //  System.out.println("you are an adult");
            
-        }
-        else{
-            System.out.println("you are not an adult");
-        }
+      //  }
+        //else{
+         //   System.out.println("you are not an adult");
+       // }
+       int x= sc.nextInt();
+       if (x % 2 == 0){
+         System.out.println("even");
+         
+      }
+      else{
+         System.out.println("odd");
+      }
     }
 }
